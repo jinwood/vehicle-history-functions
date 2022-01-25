8aa6f9df2ad40630f3721f350710363ee7c9b673
jinwood <jinw@protonmail.com>
2022-01-25 22:16:52 +0000

    Fix export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,26 @@
 import * as admin from "firebase-admin";
 import { UserRecord } from "firebase-functions/v1/auth";
+const functions = require("firebase-functions");
 
 admin.initializeApp();
 
 const db = admin.firestore();
 
 // creates a document with ID -> uid in the users collection
-export const createProfile = (userRecord: UserRecord, context: any) => {
-  const { email, phoneNumber, uid } = userRecord;
+exports.createUser = functions.auth
+  .user()
+  .onCreate(async (user: UserRecord) => {
+    const { email, phoneNumber, uid } = user;
 
-  return db
-    .collection("users")
-    .doc(uid)
-    .set({
-      email,
-      phoneNumber,
-      uid,
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
+    return db
+      .collection("users")
+      .doc(uid)
+      .set({
+        email,
+        phoneNumber,
+        uid,
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
